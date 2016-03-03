6c6b3dab4693dc43a1fd6726060eedb4d6c7177c
Gerwin Bastiaansen <gerwinou@gmail.com>
2016-03-03 19:14:08 +0100

    updates

--- R/jsonToExcel.R
+++ R/jsonToExcel.R
@@ -1,12 +1,8 @@
 setwd("~/Downloads/IGI_templates/")
 
-library(rjson)
 library(jsonlite)
-library(RCurl)
-#library(RJSONIO)
 
 json_file <- "http://uinames.com/api/?region=netherlands&amount=250"
 document <- fromJSON(json_file)
+document<-setNames(document,c("NAME","SURNAME", "SEX", "NATION"))
 
-#dat <- do.call(rbind, lapply(document$features, 
-                            # function(x) data.frame(x$properties)))
