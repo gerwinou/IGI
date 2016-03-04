f916a09d0a52469d104b7e55b41c7b5889413cc0
Gerwin Bastiaansen <gerwinou@gmail.com>
2016-03-04 15:28:03 +0100

    ready for upload in IGI
    10 users from 10 countries, including Cyrillic characters

--- R/jsonToExcel.R
+++ R/jsonToExcel.R
@@ -4,8 +4,10 @@ library(jsonlite)
 library(WriteXLS)
 library(random)
 
-df <-randomNumbers(n=90,min=10000,max=90000,col=1)
-
+empids <-randomNumbers(n=90,min=10000,max=90000,col=1)
+empids<-setNames(empids,c("CODE"))
+orgs <- randomNumbers(n=90,min=1,max=10,col=1)
+orgs <- setNames(orgs,c("OU"))
 json_file1 <- "http://uinames.com/api/?region=netherlands&amount=10"
 json_file2 <- "http://uinames.com/api/?region=france&amount=10"
 json_file3 <- "http://uinames.com/api/?region=germany&amount=10"
@@ -15,6 +17,7 @@ json_file6 <- "http://uinames.com/api/?region=india&amount=10"
 json_file7 <- "http://uinames.com/api/?region=italy&amount=10"
 json_file8 <- "http://uinames.com/api/?region=russia&amount=10"
 json_file9 <- "http://uinames.com/api/?region=united%20states&amount=10"
+json_file10 <- "http://uinames.com/api/?region=denmark&amount=10"
 
 d1 <- fromJSON(json_file1)
 d2 <- fromJSON(json_file2)
@@ -25,11 +28,10 @@ d6 <- fromJSON(json_file6)
 d7 <- fromJSON(json_file7)
 d8 <- fromJSON(json_file8)
 d9 <- fromJSON(json_file9)
+d10 <- fromJSON(json_file10)
 
 document<- rbind.data.frame(d1,d2,d3,d4,d5,d6,d7,d8,d9)
-document<- cbind(df, document)
-
-document<-setNames(document,c("CODE", "NAME","SURNAME", "SEX", "NATION"))
-
+document<- cbind(empids, document,orgs)
+document<-setNames(document,c("CODE", "NAME","SURNAME", "SEX", "COUNTRY", "OU"))[,c("CODE","NAME","SURNAME","SEX","OU","COUNTRY")]
 WriteXLS(document, ExcelFileName = "90randomUsers.xlsx",col.names = TRUE)
 
