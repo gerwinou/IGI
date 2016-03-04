3a7979a5317ee63567b4963907163787cb3f5940
Gerwin Bastiaansen <gerwinou@gmail.com>
2016-03-04 14:51:26 +0100

    Working now with 9 countries

--- R/jsonToExcel.R
+++ R/jsonToExcel.R
@@ -2,23 +2,34 @@ setwd("~/Downloads/IGI_templates/")
 
 library(jsonlite)
 library(WriteXLS)
-
 library(random)
-df <-randomNumbers(n=500,min=10000,max=90000,col=1)
 
+df <-randomNumbers(n=90,min=10000,max=90000,col=1)
 
-json_file1 <- "http://uinames.com/api/?region=netherlands&amount=250"
-json_file2 <- "http://uinames.com/api/?region=france&amount=250"
+json_file1 <- "http://uinames.com/api/?region=netherlands&amount=10"
+json_file2 <- "http://uinames.com/api/?region=france&amount=10"
+json_file3 <- "http://uinames.com/api/?region=germany&amount=10"
+json_file4 <- "http://uinames.com/api/?region=belgium&amount=10"
+json_file5 <- "http://uinames.com/api/?region=england&amount=10"
+json_file6 <- "http://uinames.com/api/?region=india&amount=10"
+json_file7 <- "http://uinames.com/api/?region=italy&amount=10"
+json_file8 <- "http://uinames.com/api/?region=russia&amount=10"
+json_file9 <- "http://uinames.com/api/?region=united%20states&amount=10"
 
-document <- fromJSON(json_file1)
-document <- fromJSON(json_file2)
+d1 <- fromJSON(json_file1)
+d2 <- fromJSON(json_file2)
+d3 <- fromJSON(json_file3)
+d4 <- fromJSON(json_file4)
+d5 <- fromJSON(json_file5)
+d6 <- fromJSON(json_file6)
+d7 <- fromJSON(json_file7)
+d8 <- fromJSON(json_file8)
+d9 <- fromJSON(json_file9)
 
-#document<- cbind((seq(from = 1000, to = 2000, by = ((to - from)/length.out -1)),length.out=250)), document)
-#document<- cbind(seq.int(10000,90000,length.out=250), document)
+document<- rbind.data.frame(d1,d2,d3,d4,d5,d6,d7,d8,d9)
 document<- cbind(df, document)
 
-#document<- cbind(seq.int(250), document)
 document<-setNames(document,c("CODE", "NAME","SURNAME", "SEX", "NATION"))
 
-WriteXLS(document, ExcelFileName = "250randomDutchUsers2.xlsx",col.names = TRUE)
+WriteXLS(document, ExcelFileName = "90randomUsers.xlsx",col.names = TRUE)
 
