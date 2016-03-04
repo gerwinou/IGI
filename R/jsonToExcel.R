06db1dc3e324a1511ac004bbb32970c62eefba6c
Gerwin Bastiaansen <gerwinou@gmail.com>
2016-03-04 10:41:34 +0100

    included french (with issues)
    Having issues with accents

--- R/jsonToExcel.R
+++ R/jsonToExcel.R
@@ -4,11 +4,14 @@ library(jsonlite)
 library(WriteXLS)
 
 library(random)
-df <-randomNumbers(n=250,min=10000,max=90000,col=1)
+df <-randomNumbers(n=500,min=10000,max=90000,col=1)
 
 
-json_file <- "http://uinames.com/api/?region=netherlands&amount=250"
-document <- fromJSON(json_file)
+json_file1 <- "http://uinames.com/api/?region=netherlands&amount=250"
+json_file2 <- "http://uinames.com/api/?region=france&amount=250"
+
+document <- fromJSON(json_file1)
+document <- fromJSON(json_file2)
 
 #document<- cbind((seq(from = 1000, to = 2000, by = ((to - from)/length.out -1)),length.out=250)), document)
 #document<- cbind(seq.int(10000,90000,length.out=250), document)
